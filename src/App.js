a58325e11d590544ab37d2afd3fb76f768169289
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-09-08 14:50:49 -0400

    default page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-full flex flex-col justify-center items-center">
+      <div className='w-screen bg-periwinkle p-8 flex justify-center items-center'>
+        <div className='mx-8 relative profile-hover rounded-full'>
+          <img src={'/LinkedinProfile.jpeg'} className='icon-hover rounded-full w-34 h-34 mx-auto border-2 opacity-1' alt='Tiffany Zhu' />
+          <div className='text-container opacity-0 rounded-full border-2 border-whit absolute inset-0 flex justify-center items-center'>
+            <p className='text-4xl text-center cursor-normal'>Let's connect on <a href='https://www.linkedin.com/in/tiffany-zhu-tz/' target='_blank' rel='noreferrer' className='hover:underline'>Linkedin</a>!</p>
+          </div>
+        </div>
+        <div className=''>
+          <h1 className='font-bold text-2xl'>I'm Tiffany Zhu 👋🏼</h1>
+          <p className='text-lg pt-4'>
+            Hello World! Welcome to my personal website 🤩
+            <br/>I am a senior at CUNY's Hunter College majoring in Computer Science as an aspiring web and software developer.
+            <br/>I am passionate about creating and designing websites and applications that are user-friendly and accessible to all.
+          </p>
+        </div>
+      </div>
+      <div>
+          <p className='text-4xl font-bold pt-20'>More Content On It's Way 🚧</p>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'periwinkle': '#CDC7E5',
+        'pomp-and-power': '#885A89',
+        'cambridge-blue': '#8FAD88',
+      },
+      width: {
+        '9/10vw': '90vw',
+      }
+    },
+  },
+  plugins: [],
+}
+
